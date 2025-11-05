3a6a312cf17de25eb961df19aebf0d833bac63d4
Noel Odero <anoel.odero@gmail.com>
2025-11-05 13:18:59 +0200

    Add Firebase configuration files and set up storage emulator for local development

--- bookswap/lib/main.dart
+++ bookswap/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart' show kDebugMode;
 
 import 'firebase_options.dart';
 import 'app.dart';
@@ -11,6 +13,15 @@ import 'providers/swap_provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // During local development use the Firebase Storage emulator to avoid CORS/billing
+  if (kDebugMode) {
+    try {
+      FirebaseStorage.instance.useStorageEmulator('localhost', 9199);
+    } catch (_) {
+      // ignore: avoid_print
+      print('Could not connect to Storage emulator');
+    }
+  }
   runApp(const MyApp());
 }
 
