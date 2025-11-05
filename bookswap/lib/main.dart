f3de485f1104a363d8edf4abe7b4890c7fcc8819
Noel Odero <anoel.odero@gmail.com>
2025-11-05 10:10:39 +0200

    Refactor main.dart to initialize Firebase with options and set up MultiProvider for state management

--- bookswap/lib/main.dart
+++ bookswap/lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
+import 'app.dart';
+import 'providers/auth_provider.dart';
+import 'providers/books_provider.dart';
+import 'providers/swap_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -13,6 +19,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => BooksProvider()),
+        ChangeNotifierProvider(create: (_) => SwapProvider()),
+      ],
+      child: const BookSwapApp(),
+    );
   }
 }
